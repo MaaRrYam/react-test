453fced9b90ad183fe64d3beac4bf9dd3246b6db
Uzair Asif <uzairasif729@gmail.com>
2023-09-14 12:39:31 +0500

    added bottom navigation added react buttons

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {StyleSheet, View} from 'react-native';
-
-import {SignIn, RequestAccess} from '@/screens';
 import {COLORS} from '@/constants';
-import {SelectRole, RequestAccessComplete} from '@/screens';
+import MyTabs from '../components/Drawer';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,13 +10,7 @@ const AppNavigation = () => {
   return (
     <View style={styles.container}>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="RequestAccess" component={RequestAccess} />
-        <Stack.Screen name="SelectRole" component={SelectRole} />
-        <Stack.Screen
-          name="RequestAccessComplete"
-          component={RequestAccessComplete}
-        />
+        <Stack.Screen name="MyTabs" component={MyTabs} />
       </Stack.Navigator>
     </View>
   );
