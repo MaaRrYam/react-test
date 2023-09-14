4c61e5d83d34adc2a8e5fb456909fdfc53784862
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2023-09-14 08:34:25 +0500

    Screens added in auth stack

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -6,7 +6,16 @@ import {StyleSheet, View} from 'react-native';
 import {SignIn, RequestAccess} from '@/screens';
 import {COLORS} from '@/constants';
 import {SelectRole, RequestAccessComplete} from '@/screens';
-import {GetStarted, Home} from '../screens';
+import {
+  Education,
+  EmploymentStatus,
+  Experience,
+  GetStarted,
+  Home,
+  Industry,
+  OnboardingCompleted,
+  SalaryExpectations,
+} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +24,19 @@ const AppNavigation = () => {
     <NavigationContainer>
       <View style={styles.container}>
         <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="GetStarted" component={GetStarted} />
+          <Stack.Screen name="Education" component={Education} />
+          <Stack.Screen name="Industry" component={Industry} />
+          <Stack.Screen name="Experience" component={Experience} />
+          <Stack.Screen name="EmploymentStatus" component={EmploymentStatus} />
+          <Stack.Screen
+            name="SalaryExpectations"
+            component={SalaryExpectations}
+          />
+          <Stack.Screen
+            name="OnboardingCompleted"
+            component={OnboardingCompleted}
+          />
           <Stack.Screen name="SignIn" component={SignIn} />
           <Stack.Screen name="RequestAccess" component={RequestAccess} />
           <Stack.Screen name="SelectRole" component={SelectRole} />
@@ -23,7 +45,6 @@ const AppNavigation = () => {
             component={RequestAccessComplete}
           />
           <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="GetStarted" component={GetStarted} />
         </Stack.Navigator>
       </View>
     </NavigationContainer>
