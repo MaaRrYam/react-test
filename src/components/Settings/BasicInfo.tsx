0fb85e75ad209871d8241c37e67e31e0b033ccc8
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2023-11-10 22:05:48 +0500

    Update BasicInfo.tsx

--- src/components/Settings/BasicInfo.tsx
+++ src/components/Settings/BasicInfo.tsx
@@ -5,7 +5,7 @@ import {PrimaryButton} from '../Buttons';
 import {getScreenDimensions, getUID} from '@/utils/functions';
 import DatePicker from 'react-native-date-picker';
 import {SettingBasicInfoUpdateInterface} from '@/interfaces';
-import {Timestamp} from 'firebase/firestore';
+// import {Timestamp} from 'firebase/firestore';
 import ProfileService from '@/services/profile';
 import ToastService from '@/services/toast';
 import useUserManagement from '@/hooks/useUserManagement';
@@ -51,9 +51,7 @@ const BasicInfo = () => {
         <Input
           placeholder="Date of Birth"
           onChangeText={() => {}}
-          value={
-            user?.dateOfBirth ? user?.dateOfBirth : dateOfBirth.toDateString()
-          }
+          value={dateOfBirth.toDateString()}
           onPress={() => setOpen(true)}
         />
         <DatePicker
